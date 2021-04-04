3a353886d2a5692554d035c8e227f48d1bf6286a
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-04-04 17:13:12 -0300

    create check_win method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class Game
   @@player1_score = 0
   @@player2_score = 0
-  @@positions = [1,2,3,4,5,6,7,8,9]
+  @@positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
@@ -14,20 +14,31 @@ respectivamente."
   end
 
   def check_win
-    
+    @@positions.each_index do |i|
+      if i + 1 % 3 != 1
+        return true if @@positions.at(i) == @@positions.at(i+1) && @@positions.at(i+1) == @@positions.at(i+2)
+      elsif i + 1 < 4
+        return true if @@positions.at(i) == @@positions.at(i+3) && @@positions.at(i+3) == @@positions.at(i+6)
+      end
+    end
+    return true if @@positions.at(0) == @@positions.at(4) && @@positions.at(4) == @@positions.at(8)
+    return true if @@positions.at(2) == @@positions.at(4) && @@positions.at(4) == @@positions.at(6)
+
+    false
   end
 
   def show_display
     puts " #{@@positions[0]} | #{@@positions[1]} | #{@@positions[2]} "
-    puts separator = '-----------'
+    puts '-----------'
     puts " #{@@positions[3]} | #{@@positions[4]} | #{@@positions[5]} "
-    puts separator
+    puts '-----------'
     puts " #{@@positions[6]} | #{@@positions[7]} | #{@@positions[8]} "
   end
 end
 
 class Player
-  attr_accessor :name, :symbol
+  attr_reader :name, :symbol
+
   def initialize(name)
     @name = name
     @symbol = name[0].capitalize
@@ -35,4 +46,5 @@ class Player
 end
 
 game = Game.new(Player.new('lauro'), Player.new('duda'))
-game.scaffold
+game.show_display
+puts game.check_win
