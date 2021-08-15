9b8ae48216a668c69d76cb1f4b017834a5f83c74
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-08-15 11:56:19 -0300

    implement winning combination tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -15,13 +15,89 @@ describe Player do
 end
 
 describe TicTacToe do
+  subject(:game_position) { described_class.new(p1, p2) }
+  let(:p1) { instance_double(Player, name: 'Lauro') }
+  let(:p2) { instance_double(Player, name: 'You') }
   
-  describe '#initialize' do
-    context 'when creating a game with two players'
-      subject(:game) { described_class.new(p1, p2)}
-      let(:p1) { instance_double(Player, value: 'Lauro')}
-      let(:p2) { instance_double(Player, value: 'You')}
+  describe '#game_over' do
+    context 'when the board top row is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, ['L', 'L', 'L', 3, 4, 5, 6, 7, 8])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
 
-      it ''
+    context 'when the board middle row is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, [0, 1, 2, 'L', 'L', 'L', 6, 7, 8])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
+
+    context 'when the board bottom row is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, [0, 1, 2, 3, 4, 5, 'L', 'L', 'L'])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
+
+    context 'when the board left column is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, ['L', 1, 2, 'L', 4, 5, 'L', 7, 8])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
+
+    context 'when the board middle column is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, [0, 'L', 2, 3, 'L', 5, 6, 'L', 8])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
+
+    context 'when the board right column is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, [0, 1, 'L', 3, 4, 'L', 6, 7, 'L'])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
+
+    context 'when the board first diagonal is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, ['L', 1, 2, 3, 'L', 5, 6, 7, 'L'])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
+
+    context 'when the board second diagonal is L L L' do
+      before do
+        game_position.instance_variable_set(:@positions, [0, 1, 'L', 3, 'L', 5, 'L', 7, 8])
+      end
+      
+      it 'is game over' do
+        expect(game_position.game_over).to be_truthy
+      end
+    end
   end
 end
