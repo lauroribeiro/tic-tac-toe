83645830bf3e6335179dc15ed73898bd8896ffae
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-08-15 10:27:05 -0300

    rename Game class to TicTacToe

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,9 @@
-require_relative 'game.rb'
+require_relative 'tic_tac_toe.rb'
 require_relative 'player.rb'
 
 puts "Player 1, choose your nickname:\n"
 player1 = gets.chomp
 puts "Player 2, choose your nickname:\n"
 player2 = gets.chomp
-game = Game.new(Player.new(player1), Player.new(player2))
+game = TicTacToe.new(Player.new(player1), Player.new(player2))
 game.play
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,2 +1,27 @@
-require_relative '../tic_tac_toe.rb'
+require_relative '../lib/tic_tac_toe.rb'
+require_relative '../lib/player.rb'
 
+describe Player do
+  subject(:player) { described_class.new('Lauro')}
+  describe '#new' do
+    it 'is named Lauro' do 
+      expect(player.name).to eq('Lauro')
+    end
+
+    it 'has a symbol L' do
+      expect(player.symbol).to eq('L')
+    end
+  end
+end
+
+describe TicTacToe do
+  
+  describe '#initialize' do
+    context 'when creating a game with two players'
+      subject(:game) { described_class.new(p1, p2)}
+      let(:p1) { instance_double(Player, value: 'Lauro')}
+      let(:p2) { instance_double(Player, value: 'You')}
+
+      it ''
+  end
+end
